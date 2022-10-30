1a075078bb5d4fbc1ab3c7bd51c9e16d99177723
ankita-m18 <106266070+ankita-m18@users.noreply.github.com>
2022-10-30 23:18:37 +0530

    adding patient_id

--- src/main/java/com/jcg/mongodb/servlet/Registration.java
+++ src/main/java/com/jcg/mongodb/servlet/Registration.java
@@ -1,14 +1,26 @@
 package com.jcg.mongodb.servlet;
 
-import com.mongodb.client.MongoCollection; 
-import com.mongodb.client.MongoDatabase; 
-import org.bson.Document; 
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
 import com.mongodb.MongoClient; 
 //import com.mongodb.MongoCredential;
 import com.mongodb.MongoException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,13 +32,15 @@ public class Registration extends HttpServlet {
 	public static String aadhaar;
 
 	private static final long serialVersionUID = 1L;
+	String id="";
 
 	// This method is called by the servlet container to process a 'post' request
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		handleRequest(request, response);
 	}
 
-	public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException 
+	{
 
 		// Reading post parameters from the request
 		String firstname = request.getParameter("firstname");
@@ -37,13 +51,15 @@ public class Registration extends HttpServlet {
 		String birthday = request.getParameter("birthday");
 		int pincode = Integer.parseInt(request.getParameter("pincode"));
 
+		String pid="";
+		//Long pid2=0L;
 		// Creating a Mongo client
 	    
 	    
 	    try (MongoClient mongo = new MongoClient( "localhost" , 27017 ))
 	    {
 	    	// Accessing the database 
-		    MongoDatabase database = mongo.getDatabase("NcdDb"); 
+		    MongoDatabase database = mongo.getDatabase("ncdjava"); 
 		    
 		    
 		    try
@@ -59,26 +75,102 @@ public class Registration extends HttpServlet {
 		    MongoCollection<Document> collection = database.getCollection("PatientInfo"); 
 		    System.out.println("Collection PatientInfo selected successfully"); 
 		    
+		    
+		    //insertion of the values in the field
 		    try
 		    {
-		    	Document document = new Document("First Name", firstname)
-		    			.append("Last Name", lastname)
-		    			.append("Gender", gender)
-		    			.append("Aadhaar UID", aadhaar)
-		    			.append("Phone No", phone)
-		    			.append("DOB", birthday)
-		    			.append("Pincode", pincode);
+		    	
+		    	List<Document> results = new ArrayList<>();
+		    	FindIterable<Document> iterable = collection.find();
+		    	iterable.into(results);
+		    	
+		    	if(!results.isEmpty())
+		    	{
+		    		for(Document document: results) 
+		    		{
+		    		    
+		    			 long  min = 99999999999999L;
+		    		     long  max = 10000000000000L;
+		    		     long random_int = (long)(Math.random() * (max - min + 1) + min);
+		    		     id = Long.toString(random_int);
 		    			
-		    			//Inserting document into the collection
-		    			collection.insertOne(document);	
+		    			//id = String.valueOf(Math.random() * 1000000000000000L);
+		    			if(id!=document.get("_id"))
+		    			{
+		    				 Document doc = new Document("Patient id", id)
+		    			    			.append("First Name", firstname)
+		    			    			.append("Last Name", lastname)
+		    			    			.append("Gender", gender)
+		    			    			.append("Aadhaar UID", aadhaar)
+		    			    			.append("Phone No", phone)
+		    			    			.append("DOB", birthday)
+		    			    			.append("Pincode", pincode);
+		    			    			
+		    			    			//Inserting document into the collection
+		    			    			collection.insertOne(doc);
+		    			    			break;
+		    			}
+		    			else
+		    			{
+		    				continue;
+		    			}
+		    		}
+		    	}
+		    	else
+		    	{
+		    		
+		    		long  min = 99999999999999L;
+	    		    long  max = 10000000000000L;
+	    		    long random_int = (long)(Math.random() * (max - min + 1) + min);
+	    		    id = Long.toString(random_int);
+		    		//id = String.valueOf(Math.random() * 1000000000000000L);
+		    		Document doc = new Document("Patient id", id)
+			    			.append("First Name", firstname)
+			    			.append("Last Name", lastname)
+			    			.append("Gender", gender)
+			    			.append("Aadhaar UID", aadhaar)
+			    			.append("Phone No", phone)
+			    			.append("DOB", birthday)
+			    			.append("Pincode", pincode);
+			    			
+			    			//Inserting document into the collection
+			    			collection.insertOne(doc);
+		    	}
+		            
+		       
 		    			
-		    			request.getRequestDispatcher("/checklist.jsp").forward(request, response);
+		    			
 		    }
 		    catch (MongoException me) {
                 System.err.println("Unable to insert due to an error: " + me);
             }
 		    
+		    
+		    
+		    //selecting patient_id to be displayed in the next field
+		    try {
+		    	List<Document> results = new ArrayList<>();
+		    	FindIterable<Document> fi = collection.find(Filters.eq("Aadhaar UID", aadhaar));
+		    	fi.into(results);
+		    	
+		    	
+		    	pid =results.get(0).get("Patient id").toString();
+		    	
+		    	//pid2=Long.parseLong(pid);
+		    	
+		    }
+		    catch(MongoException me)
+		    {
+		    	System.err.println("Unable to select id due to an error: " + me);
+		    }
+		    
+		    //RequestDispatcher dispatcher = request.getRequestDispatcher("/checklist.jsp");
+	        request.setAttribute("pid", pid);
+			//dispatcher.forward(request, response);
+		    request.getRequestDispatcher("/checklist.jsp").forward(request, response);
+		    
 	    }
 	   	    
 	}
+	
 }
